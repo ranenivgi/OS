0834d94e8be7c33168774b3bf4daef1127be179d
ranen ivgi <ranen.ivgi@gmail.com>
2023-05-03 17:30:16 +0300

    first part ex2

--- ex2/files/ex21.c
+++ ex2/files/ex21.c
@@ -5,11 +5,12 @@
 #include <unistd.h>
 #include <ctype.h>
 
-int checkRead(int r1, int r2, int result)
+int checkRead(int r1, int r2, int result, char c1, char c2)
 {
     if (r1 < 0 || r2 < 0)
     {
-        printf("Error in: read\n");
+        perror("Error in: read\n");
+        return -1;
     }
 
     if (r1 == 0 && r2 == 0)
@@ -21,10 +22,15 @@ int checkRead(int r1, int r2, int result)
         return 1;
     }
 
-    if (r1 == 0 || r2 == 0)
+    if (r1 == 0 && (c2 != ' ' && c2 != '\n'))
     {
         return 2;
     }
+    if (r2 == 0 && (c1 != ' ' && c1 != '\n'))
+    {
+        return 2;
+    }
+    return 0;
 }
 
 int compareFiles(int first, int second)
@@ -39,37 +45,29 @@ int compareFiles(int first, int second)
 
     while (1)
     {
-        check = checkRead(r1, r2, result);
-        if (check > 0)
+        check = checkRead(r1, r2, result, c1, c2);
+        if (check)
         {
             return check;
         }
 
         if (c1 == c2)
         {
+            r1 = read(first, &c1, 1);
+            r2 = read(second, &c2, 1);
             continue;
         }
 
-        if (c1 == ' ' || c1 == '\n')
+        if ((c1 == ' ' || c1 == '\n') && r1 != 0)
         {
             r1 = read(first, &c1, 1);
-            check = checkRead(r1, r2, result);
-            if (check > 0)
-            {
-                return check;
-            }
             result = 3;
             continue;
         }
 
-        if (c2 == ' ' || c2 == '\n')
+        if ((c2 == ' ' || c2 == '\n') && r2 != 0)
         {
             r2 = read(second, &c2, 1);
-            check = checkRead(r1, r2, result);
-            if (check > 0)
-            {
-                return check;
-            }
             result = 3;
             continue;
         }
@@ -77,11 +75,17 @@ int compareFiles(int first, int second)
         if (tolower(c1) == tolower(c2))
         {
             result = 3;
+            r1 = read(first, &c1, 1);
+            r2 = read(second, &c2, 1);
             continue;
         }
 
-        r1 = read(first, &c1, 1);
-        r2 = read(second, &c2, 1);
+        if (c1 != c2)
+        {
+            return 2;
+        }
+
+        
     }
     return result;
 }
@@ -96,15 +100,22 @@ int main(int argc, char *argv[])
     int firstFile = open(argv[1], 'r');
     if (firstFile < 0)
     {
+        perror("Error in: open");
         return -1;
     }
     int secondFile = open(argv[2], 'r');
     if (secondFile < 0)
     {
+        perror("Error in: open");
         return -1;
     }
 
-    close(firstFile);
-    close(secondFile);
-    return compareFiles(firstFile, secondFile);
+    int result = compareFiles(firstFile, secondFile);
+    if (close(firstFile) < 0 || close(secondFile) < 0)
+    {
+        perror("Error in: close");
+        return -1;
+    }
+
+    return result;
 }
