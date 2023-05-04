a20b6014311079a99c5f9d81b2e92dc8ad0b0a16
ranen ivgi <ranen.ivgi@gmail.com>
2023-05-04 21:56:27 +0300

    updated files

--- ex2/files/students/Excellent_gets_100/great5.c.txt.txt.txt.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-	int n1, n2;
-	printf("Please enter two numbers\n");
-	scanf ("%d %d",&n1, &n2);
-	printf("%d", n1+n2);
-}
--- ex2/files/students/Wrong_output_gets_50/Example.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("This is not the correct output!\n");
-	return 0;
-}
