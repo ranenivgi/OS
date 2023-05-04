8c2c13ec8032a55cacace765be7691f6c3e7d640
ranen ivgi <ranen.ivgi@gmail.com>
2023-05-04 17:40:54 +0300

    ex 2 part 2 completed compilation milestone

--- ex2/files/ex22.c
+++ ex2/files/ex22.c
@@ -5,8 +5,20 @@
 #include <sys/file.h>
 #include <dirent.h>
 #include <stdio.h>
+#include <sys/wait.h>
 
-int is_file(const char *path)
+int isFolder(const char *path)
+{
+    struct stat info;
+    if (stat(path, &info) == -1)
+    {
+        // Error occurred while trying to get file info
+        return 0;
+    }
+    return S_ISDIR(info.st_mode);
+}
+
+int isFile(const char *path)
 {
     struct stat info;
 
@@ -18,15 +30,99 @@ int is_file(const char *path)
     return S_ISREG(info.st_mode);
 }
 
-int is_folder(const char *path)
+int compile(char *compile_file_path, int errors, struct dirent *student_name, int results)
 {
-    struct stat info;
-    if (stat(path, &info) == -1)
+    pid_t pid;
+    int status;
+
+    pid = fork();
+
+    if (pid == -1)
     {
-        // Error occurred while trying to get file info
+        write(2, "Error in: fork\n", strlen("Error in: fork\n"));
         return 0;
     }
-    return S_ISDIR(info.st_mode);
+    // child process, compile the .c file
+    else if (pid == 0)
+    {
+        if (dup2(errors, 1) == -1 || dup2(errors, 2) == -1)
+        {
+            write(2, "Error in: dup2\n", strlen("Error in: dup2\n"));
+            exit(-1);
+        }
+
+        char *cmp[] = {"gcc", "-o", "compiled_file.out", compile_file_path, NULL};
+        if (execvp(cmp[0], cmp) == -1)
+        {
+            write(2, "Error in: execvp\n", strlen("Error in: execvp\n"));
+            return 0;
+        }
+    }
+    // parent process, waiting for the compilation and writes the error if exists
+    else
+    {
+        if (wait(&status) == -1)
+        {
+            write(2, "Error in: wait\n", strlen("Error in: wait\n"));
+            return 0;
+        }
+
+        if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
+        {
+            write(results, student_name->d_name, strlen(student_name->d_name));
+            write(results, ",10,COMPILATION_ERROR\n", strlen(",10,COMPILATION_ERROR\n"));
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int rateStudent(struct dirent* student_name, char *student_folder_path, int results, int errors)
+{
+    char *opendir_err = "Error in: opendir\n";
+
+    DIR *students_dir;
+    struct dirent *file_iterator;
+
+    if ((students_dir = opendir(student_folder_path)) == NULL)
+    {
+        write(2, opendir_err, strlen(opendir_err));
+        exit(-1);
+    }
+
+    while ((file_iterator = readdir(students_dir)) != NULL)
+    {
+        // skip over the '.' and '..' entries
+        if (strcmp(file_iterator->d_name, ".") == 0 || strcmp(file_iterator->d_name, "..") == 0)
+        {
+            continue;
+        }
+
+        // get the file full path
+        char compile_file_path[150] = "";
+        strcpy(compile_file_path, student_folder_path);
+        strcat(compile_file_path, "/");
+        strcat(compile_file_path, file_iterator->d_name);
+
+        // check that the student path is a folder
+        if (!isFile(compile_file_path))
+        {
+            continue;
+        }
+        // check if the file is a c file
+        if (strcmp(file_iterator->d_name + strlen(file_iterator->d_name) - 2, ".c") != 0)
+        {
+            continue;
+        }
+
+        // compile the .c file, move to the next student if it doesn't work
+        if (!compile(compile_file_path, errors, student_name, results))
+        {
+            break;
+        }
+
+        break;
+    }
 }
 
 int main(int argc, char *argv[])
@@ -73,21 +169,21 @@ int main(int argc, char *argv[])
     }
 
     // make sure the students path is a folder
-    if (!is_folder(students))
+    if (!isFolder(students))
     {
         write(2, folder_err, strlen(folder_err));
         return -1;
     }
 
     // make sure the input path is a file
-    if (!is_file(input))
+    if (!isFile(input))
     {
         write(2, input_err, strlen(input_err));
         return -1;
     }
 
     // make sure the output path is a file
-    if (!is_file(output))
+    if (!isFile(output))
     {
         write(2, output_err, strlen(output_err));
         return -1;
@@ -117,7 +213,7 @@ int main(int argc, char *argv[])
     }
 
     DIR *students_dir;
-    struct dirent *one_student_dir;
+    struct dirent *student_name;
 
     if ((students_dir = opendir(students)) == NULL)
     {
@@ -126,10 +222,10 @@ int main(int argc, char *argv[])
     }
 
     // looping through the directory, printing the directory entry name
-    while ((one_student_dir = readdir(students_dir)) != NULL)
+    while ((student_name = readdir(students_dir)) != NULL)
     {
         // skip over the '.' and '..' entries
-        if (strcmp(one_student_dir->d_name, ".") == 0 || strcmp(one_student_dir->d_name, "..") == 0)
+        if (strcmp(student_name->d_name, ".") == 0 || strcmp(student_name->d_name, "..") == 0)
         {
             continue;
         }
@@ -138,18 +234,19 @@ int main(int argc, char *argv[])
         char student_folder_path[150] = "";
         strcpy(student_folder_path, students);
         strcat(student_folder_path, "/");
-        strcat(student_folder_path, one_student_dir->d_name);
+        strcat(student_folder_path, student_name->d_name);
 
         // check that the student path is a folder
-        if (!is_folder(student_folder_path))
+        if (!isFolder(student_folder_path))
         {
             continue;
         }
-        
-        printf("%s\n", one_student_dir->d_name);
+
+        rateStudent(student_name, student_folder_path, results, errors);
+        printf("%s\n", student_folder_path);
     }
     closedir(students_dir);
-    close(input);
+    close(input_dir);
     close(results);
     close(errors);
     return 0;
